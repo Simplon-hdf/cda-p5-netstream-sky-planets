121b19e41d4d6cedc948263e3490fcfcc6e22ca4
Emerick <decdu59@live.fr>
2025-04-29 14:07:18 +0200

    fix(script_create.sql.sql): add modification date columns to all tables

--- script_create.sql
+++ script_create.sql
@@ -4,6 +4,7 @@ CREATE TABLE realisateur (
     id_realisateur UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nom_realisateur VARCHAR(50) NOT NULL,
     prenom_realisateur VARCHAR(50) NOT NULL
+    date_modification_realisateur TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE film (
@@ -12,6 +13,7 @@ CREATE TABLE film (
     date_de_sortie DATE NOT NULL,
     duree TIME NOT NULL,
     id_realisateur UUID NOT NULL,
+    date_modification_film TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_realisateur) REFERENCES realisateur(id_realisateur)
 );
 
@@ -19,6 +21,7 @@ CREATE TABLE role_film (
     id_role UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nom_role VARCHAR(150) NOT NULL,
     id_film UUID NOT NULL,
+    date_modification_role_film TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_film) REFERENCES film(id_film)
 );
 
@@ -26,7 +29,9 @@ CREATE TABLE acteur (
     id_acteur UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nom_acteur VARCHAR(50) NOT NULL,
     prenom_acteur VARCHAR(50) NOT NULL,
-    date_de_naissance DATE NOT NULL
+    date_de_naissance DATE NOT NULL,
+    date_modification_acteur TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
 );
 
 CREATE TABLE cinephile (
@@ -34,12 +39,14 @@ CREATE TABLE cinephile (
     nom_cinephile VARCHAR(50) NOT NULL,
     prenom_cinephile VARCHAR(50) NOT NULL,
     email VARCHAR(320) UNIQUE NOT NULL,
-    mot_de_passe VARCHAR(72) NOT NULL
+    mot_de_passe VARCHAR(72) NOT NULL,
+    date_modification_cinephile TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE role_preferer (
     id_cinephile UUID NOT NULL,
     id_role UUID NOT NULL,
+    date_modification_role_preferer TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_cinephile) REFERENCES cinephile(id_cinephile),
     FOREIGN KEY (id_role) REFERENCES role_film(id_role)
 );
@@ -47,6 +54,7 @@ CREATE TABLE role_preferer (
 CREATE TABLE film_preferer (
     id_cinephile UUID NOT NULL,
     id_film UUID NOT NULL,
+    date_modification_film_preferer TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_cinephile) REFERENCES cinephile(id_cinephile),
     FOREIGN KEY (id_film) REFERENCES film(id_film)
 );
@@ -54,6 +62,7 @@ CREATE TABLE film_preferer (
 CREATE TABLE jouer (
     id_acteur UUID NOT NULL,
     id_role UUID NOT NULL,
+    date_modification_jouer TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_acteur) REFERENCES acteur(id_acteur),
     FOREIGN KEY (id_role) REFERENCES role_film(id_role)
 );
@@ -64,5 +73,5 @@ CREATE TABLE archive (
     champ_modifie VARCHAR(50) NOT NULL,
     ancienne_valeur TEXT NOT NULL,
     nouvelle_valeur TEXT NOT NULL,
-    date_modification TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    date_modification_archive TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
